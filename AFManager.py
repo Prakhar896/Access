83fe5470f5573af7090bc8379ba73aff187b7e15
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2022-03-16 14:26:27 +0800

    added delete method for AFM and improved error checking

--- AFManager.py
+++ AFManager.py
@@ -7,8 +7,12 @@ class AFManager:
         if os.path.isdir(os.path.join(os.getcwd(), 'AccessFolders', username)):
             return AFMError.folderAlreadyRegistered
         
-        os.mkdir(os.path.join(os.getcwd(), 'AccessFolders', username))
-        print("AFM: User {} registered an Access Folder.")
+        try:
+            os.mkdir(os.path.join(os.getcwd(), 'AccessFolders', username))
+            print("AFM: User {} registered an Access Folder.")
+        except Exception as e:
+            print("AFMError: {}".format(e))
+            return AFMError.unknownError
         return "Access Folder for {} registered.".format(username)
 
     @staticmethod
@@ -17,6 +21,19 @@ class AFManager:
             return True
         else:
             return False
+
+    @staticmethod
+    def deleteFolder(username):
+
+        if os.path.isdir(os.path.join(os.getcwd(), 'AccessFolders', username)):
+            try:
+                os.rmdir(os.path.isdir(os.path.join(os.getcwd(), 'AccessFolders', username)))
+            except Exception as e:
+                print("AFMError: {}".format(e))
+                return AFMError.unknownError
+        else:
+            return AFMError.folderDoesNotExist
+
     
 
 class AFMError(Exception):
@@ -24,11 +41,15 @@ class AFMError(Exception):
         self.message = message
 
     folderAlreadyRegistered = "AFMError: The Access Folder is already registered for the username."
+    unknownError = "AFMError: There was an unknown error in performing the AFM action. Check console for more information."
+    folderDoesNotExist = "AFMError: No such Access Folder exists."
 
     @staticmethod
     def checkIfErrorMessage(msg):
         msgsArray = [
-            AFMError.folderAlreadyRegistered
+            AFMError.folderAlreadyRegistered,
+            AFMError.unknownError,
+            AFMError.folderDoesNotExist
         ]
         
         if msg in msgsArray:
