5ef8153e7c42526134a73146f85493290c11474a
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2022-05-29 22:10:32 +0800

    fixed the bug from the previous commit properly this time

--- supportJSFiles/updateEmailConfirmation.js
+++ supportJSFiles/updateEmailConfirmation.js
@@ -1,11 +1,5 @@
-function redirectEmailConfirmation() {
-    var arrayURL = url.split("/");
-    arrayURL = arrayURL.slice(0, 7).join('/') + "/settings/idInfo/updateEmail"
-    window.location.href = arrayURL;
-}
-
 function sendOTPVerification() {
-
+    
 }
 
 function updateEmail() {
