0be3e0cd165e2ae7bafecd9e367ca658ba04d614
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2022-05-29 22:05:16 +0800

    fixed 'url already defined' bug

--- supportJSFiles/updateEmailConfirmation.js
+++ supportJSFiles/updateEmailConfirmation.js
@@ -1,3 +1,9 @@
+function redirectEmailConfirmation() {
+    var arrayURL = url.split("/");
+    arrayURL = arrayURL.slice(0, 7).join('/') + "/settings/idInfo/updateEmail"
+    window.location.href = arrayURL;
+}
+
 function sendOTPVerification() {
 
 }
