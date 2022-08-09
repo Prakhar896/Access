6894516a6bb9965bf83019e34ca82bd7907c7ddd
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2022-08-09 14:55:19 +0800

    added rereadData endpoint for devMode
    working on passwordReset process now

---
+++ identityMeta.py
@@ -0,0 +1,26 @@
+from main import *
+
+@app.route('/identity/create')
+def createIdentityPage():
+  return render_template('createIdentity.html')
+
+@app.route('/identity/login/')
+def loginIdentityPage():
+  if 'email' not in request.args:
+    return render_template('loginIdentity.html', email="")
+  else:
+    return render_template('loginIdentity.html', email=request.args['email'])
+
+@app.route('/identity/logout/')
+def logout():
+  if 'username' not in request.args:
+    flash('Username was not provided for identity logout. Failed to perform identity logout.')
+    return redirect(url_for('processError'))
+  elif 'authToken' not in request.args:
+    flash('Authentication token was not provided for identity logout. Failed to perform identity logout.')
+    return redirect(url_for('processError'))
+  return render_template('logout.html', username=request.args['username'])
+
+@app.route('/identity/passwordReset')
+def passwordReset():
+    return
--- main.py
+++ main.py
@@ -85,26 +85,34 @@ def updateAnalytics():
 def homepage():
   return fileContent('homepage.html')
 
-@app.route('/identity/create')
-def createIdentityPage():
-  return render_template('createIdentity.html')
+@app.route('/system/rereadData')
+def rereadData():
+  if devMode:
+    if not os.path.isfile('accessIdentities.txt'):
+      with open('accessIdentities.txt', 'w') as f:
+        f.write("{}")
 
-@app.route('/identity/login/')
-def loginIdentityPage():
-  if 'email' not in request.args:
-    return render_template('loginIdentity.html', email="")
+    accessIdentities = json.load(open('accessIdentities.txt', 'r'))
+
+    if not os.path.isfile('certificates.txt'):
+      with open('certificates.txt', 'w') as f:
+        f_content = """{ "registeredCertificates": {}, "revokedCertificates": {}}"""
+        f.write(f_content)
+
+    CertAuthority.loadCertificatesFromFile(fileObject=open('certificates.txt', 'r'))
+
+    if not os.path.isfile('validOTPCodes.txt'):
+      with open('validOTPCodes.txt', 'w') as f:
+        f.write("{}")
+
+    validOTPCodes = json.load(open('validOTPCodes.txt', 'r'))
+
+    return "Data re-read successfully!"
   else:
-    return render_template('loginIdentity.html', email=request.args['email'])
+    return "Endpoint access forbidden. Please enable developer mode to use this feature."
 
-@app.route('/identity/logout/')
-def logout():
-  if 'username' not in request.args:
-    flash('Username was not provided for identity logout. Failed to perform identity logout.')
-    return redirect(url_for('processError'))
-  elif 'authToken' not in request.args:
-    flash('Authentication token was not provided for identity logout. Failed to perform identity logout.')
-    return redirect(url_for('processError'))
-  return render_template('logout.html', username=request.args['username'])
+# Import Identity Meta Transactions Service
+from identityMeta import *
 
 @app.route('/security/unauthorised')
 def unauthorizedPage():
