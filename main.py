a019a3c435941d95bd89ac2403237397d924f8e5
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-01-26 13:43:15 +0530

    added more extensions to allowed file extensions list

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from getpass import getpass
 
 ### APP CONFIG
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'Chute')
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xlsx'}
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'xlsx', 'heic', 'mov', 'mp4', 'docx', 'pptx', 'py', 'swift', 'js'}
 ALLOWED_EXTENSIONS_AS_LIST = [x for x in ALLOWED_EXTENSIONS]
 prepFileExtensions = ', '.join(["."+x for x in ALLOWED_EXTENSIONS_AS_LIST])
 
