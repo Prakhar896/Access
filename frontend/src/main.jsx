a159f10675aa9c6156f66cdfd54643e24b406459
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-11-01 13:17:16 +0900

    added redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,13 @@ import './index.css'
 
 import App from './App.jsx'
 
+const store = configureStore({
+    reducer: {
+        universal: universalReducer,
+        auth: authReducer
+    }
+})
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <ChakraProvider>
