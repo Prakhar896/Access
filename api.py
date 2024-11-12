6838e32b770ee01b475c20597f19e7d7186e3930
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-11-12 18:12:44 +0900

    refactored email verification code to separate function

--- api.py
+++ api.py
@@ -7,6 +7,31 @@ from decorators import *
 
 apiBP = Blueprint('api', __name__)
 
+def dispatchEmailVerification(destEmail: str, otpCode: str):
+    text = """
+    Welcome to the Access family! To finish signing up, please enter the following OTP code onto the website:
+    
+    OTP Code: {}
+    
+    Kind regards,
+    The Access Team
+    
+    THIS IS AN AUTOMATED MESSAGE DELIVERED TO YOU BY THE ACCESS PORTAL. DO NOT REPLY TO THIS EMAIL.
+    {}
+    """.format(otpCode, Universal.copyright)
+    
+    Emailer.sendEmail(
+        destEmail=destEmail,
+        subject="Verify Email | Access",
+        altText=text,
+        html=render_template(
+            "emails/otpEmail.html",
+            otpCode=otpCode,
+            copyright=Universal.copyright
+        )
+    )
+    print("Function done.")
+
 @apiBP.route("/identity/new", methods=["POST"])
 @jsonOnly
 @checkAPIKey
@@ -81,28 +106,7 @@ def newIdentity():
         return "ERROR: Failed to process request. Please try again.", 500
     
     # Dispatch OTP verification email
-    text = """
-    Welcome to the Access family! To finish signing up, please enter the following OTP code onto the website:
-    
-    OTP Code: {}
-    
-    Kind regards,
-    The Access Team
-    
-    THIS IS AN AUTOMATED MESSAGE DELIVERED TO YOU BY THE ACCESS PORTAL. DO NOT REPLY TO THIS EMAIL.
-    {}
-    """.format(otpCode, Universal.copyright)
-    
-    Emailer.sendEmail(
-        destEmail=account.email,
-        subject="Verify Email | Access",
-        altText=text,
-        html=render_template(
-            "emails/otpEmail.html",
-            otpCode=otpCode,
-            copyright=Universal.copyright
-        )
-    )
+    dispatchEmailVerification(account.email, otpCode)
     
     return "SUCCESS: Identity created. Verify email via OTP code dispatched.", 200
 
@@ -135,7 +139,7 @@ def loginIdentity(user: Identity | None=None):
     try:
         account = Identity.load(username=usernameOrEmail, email=usernameOrEmail)
         if not isinstance(account, Identity):
-            return "UERROR: Invalid credentials.", 400
+            return "UERROR: Username or email does not exist.", 404
     except Exception as e:
         Logger.log("IDENTITY LOGIN ERROR: Failed to find identity. Error: {}".format(e))
         return "ERROR: Failed to process request. Please try again.", 500
@@ -191,7 +195,7 @@ def verifyOTP():
     try:
         account = Identity.load(username=usernameOrEmail, email=usernameOrEmail)
         if not isinstance(account, Identity):
-            return "UERROR: Invalid credentials.", 401
+            return "UERROR: Account not found.", 404
     except Exception as e:
         Logger.log("IDENTITY OTP VERIFY ERROR: Failed to find identity. Error: {}".format(e))
         return "ERROR: Failed to process request. Please try again.", 500
@@ -218,8 +222,44 @@ def verifyOTP():
     
     return "SUCCESS: Email verified successfully.", 200
 
-# @apiBP.route("/identity/resendEmailVerification", methods=["POST"])
-# @checkAPIKey
-# @checkSession(provideIdentity=True)
-# def resendEmailVerification(user: Identity | None=None):
-#     pass
+@apiBP.route("/identity/resendEmailVerification", methods=["POST"])
+@checkAPIKey
+@checkSession(provideIdentity=True)
+def resendEmailVerification(user: Identity | None=None):
+    if not isinstance(user, Identity):
+        if not request.is_json or "usernameOrEmail" not in request.json:
+            return "ERROR: Invalid request.", 400
+        
+        # Since the user is not logged in, retrieve account manually
+        
+        ## Check if email is even valid
+        usernameOrEmail = request.json["usernameOrEmail"].strip()
+        if "@" in usernameOrEmail:
+            if not re.match("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", usernameOrEmail):
+                return "UERROR: Email is not in the correct format.", 400
+        
+        ## Check if account exists
+        try:
+            user = Identity.load(username=usernameOrEmail, email=usernameOrEmail)
+            if not isinstance(user, Identity):
+                return "UERROR: Account not found.", 404
+        except Exception as e:
+            Logger.log("IDENTITY RESEND EMAIL ERROR: Failed to find identity. Error: {}".format(e))
+            return "ERROR: Failed to process request. Please try again.", 500
+        
+    if user.emailVerification.verified:
+        return "UERROR: Email already verified.", 400
+    if isinstance(user.emailVerification.dispatchTimestamp, str):
+        if (Universal.utcNow() - Universal.fromUTC(user.emailVerification.dispatchTimestamp)).total_seconds() < 60:
+            return "UERROR: Cannot resend verification email within 60 seconds of dispatch.", 400
+    
+    # Generate new OTP code and dispatch email
+    otpCode = Universal.generateUniqueID(customLength=6)
+    
+    user.emailVerification.otpCode = otpCode
+    user.emailVerification.dispatchTimestamp = Universal.utcNowString()
+    user.emailVerification.save()
+    
+    dispatchEmailVerification(user.email, otpCode)
+    
+    return "SUCCESS: Email verification OTP code dispatched.", 200
