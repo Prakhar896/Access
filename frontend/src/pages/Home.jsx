52359fd998663aac90a20808dbcd2ec2b6889067
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-11-14 19:20:18 +0900

    made homepage more mobile friendly

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import { Box, Button, Heading, Image, Spacer, Spinner, Text, useColorMode } from '@chakra-ui/react'
+import { Box, Button, Heading, Image, Spacer, Spinner, Text, useColorMode, useMediaQuery } from '@chakra-ui/react'
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import colouredLogo from '/logo/svg/logo-color.svg';
 
 function Home() {
     const [loading, setLoading] = useState(false);
+    const [limitedScreen] = useMediaQuery("(max-width: 800px)");
 
     const toggleLoading = () => {
         const newLoading = !loading;
@@ -17,13 +18,13 @@ function Home() {
     }
 
     return (
-        <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} maxW={'100%'} p={'10px'}>
-            <Box display={'flex'} flexDirection={'row'} justifyContent={'space-between'} alignItems={'center'} mt={'20%'}>
+        <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} maxW={'100%'} p={!limitedScreen ? '10px': '0px'}>
+            <Box display={'flex'} flexDirection={limitedScreen ? 'column': 'row'} justifyContent={'space-between'} alignItems={'center'} mt={'20%'}>
                 <Spacer />
                 <Image src={colouredLogo} alt={'Logo'} maxH={{ base: '100px', md: '150px', lg: '200px' }} rounded={'xl'}/>
                 <Spacer />
-                <Box display={'flex'} flexDir={'column'} justifyContent={'left'} alignItems={'left'} maxW={'50%'} p={'10px'}>
-                    <Heading as={'h1'} size={'2xl'}>Welcome to Access</Heading>
+                <Box display={'flex'} flexDir={'column'} justifyContent={'left'} alignItems={'left'} maxW={limitedScreen ? '70%': '50%'} p={!limitedScreen ? '10px': '0px'} mt={limitedScreen ? '20px': '0px'}>
+                    <Heading as={'h1'} size={{ base: 'xl', md: '2xl'}}>Welcome to Access</Heading>
                     <Text mt={'10px'}>Access is a cloud storage service for all your quick, secure and efficient storage needs.</Text>
                     <br />
                     <Text>It's just two steps: Create an account and start uploading!</Text>
