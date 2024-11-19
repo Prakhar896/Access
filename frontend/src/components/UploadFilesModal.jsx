8d56a04e533d4dd0fcc3da7406ceb615e0260bea
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-11-19 19:56:43 +0900

    added sample modal for upload files

---
+++ frontend/src/components/UploadFilesModal.jsx
@@ -0,0 +1,24 @@
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react'
+import React from 'react'
+
+function UploadFilesModal() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    return (
+        <Modal onClose={onClose} isOpen={isOpen} isCentered>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Text>Hello, world!</Text>
+                </ModalBody>
+                <ModalFooter>
+                    <Button onClick={onClose}>Close</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
+    )
+}
+
+export default UploadFilesModal
