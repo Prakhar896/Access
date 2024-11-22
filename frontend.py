9b7a6a32be0e690ad299790e80360ab13074931b
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2024-11-22 12:38:22 +0900

    added all routes in frontend

--- frontend.py
+++ frontend.py
@@ -6,6 +6,30 @@ frontendBP = Blueprint('frontend', __name__)
 def homepage():
     return render_template("index.html")
 
+@frontendBP.route('/login')
+def login():
+    return render_template("index.html")
+
+@frontendBP.route('/signup')
+def signup():
+    return render_template("index.html")
+
+@frontendBP.route('/forgotPassword')
+def forgotPassword():
+    return render_template("index.html")
+
+@frontendBP.route('/verifyEmail')
+def verifyEmail():
+    return render_template("index.html")
+
+@frontendBP.route('/portal/files')
+def files():
+    return render_template("index.html")
+
+@frontendBP.route('/portal/account')
+def account():
+    return render_template("index.html")
+
 @frontendBP.route('/s')
 def sharedFile():
     return render_template("index.html")
